69c8513d40a049bf52c33653ed29900c5a4dbc20
LeThanhDat0224041196 <lethanhdat0224@gmail.com>
2022-06-25 10:55:35 +0700

    Uploads

---
+++ reactjsbt2/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BTGlasses from './Bài tập glasses/BTGlasses';
+
+function App() {
+  return (
+    <div className="App">
+      <BTGlasses />
+    </div>
+  );
+}
+
+export default App;
---
+++ reactjsbt2/src/Bài tập glasses/BTGlasses.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class BTGlasses extends Component {
+  render() {
+    return (
+      <div>BTGlasses</div>
+    )
+  }
+}
